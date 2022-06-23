642936ffe127ac10b656fd49d50c34d78b5c5a28
Liam Cammpbell <liam22campbell@gmail.com>
2022-06-22 21:06:36 -0700

    Pass first test.

--- __tests__/rectangle.test.js
+++ __tests__/rectangle.test.js
@@ -1,1 +1,13 @@
 import Rectangle from './../src/rectangle.js';
+
+describe('Rectangle',  () => {
+  test('should correctly create a rectangle object using two sides', () => {
+    const rectangle = new Rectangle(3, 5);
+    expect(rectangle.side1).toEqual(3);
+    expect(rectangle.side2).toEqual(5);
+  });
+});
+
+  test('should correctly get the area of a rectangle object', () => {
+    const rectangle
+  })
